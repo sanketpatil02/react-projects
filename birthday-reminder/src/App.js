a43b10e7a9242222340d562354fe7f7bddf59e09
Sanket Patil <sanketpatil9902@gmail.com>
2023-09-01 20:27:01 +0530

    First commit

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h2>{people.length} Birthdays Today</h2>
+        <List people={people} />
+        <button onClick={ () => setPeople([]) }>Clear all</button>
+      </section>
+    </main>
+  )
+}
+
+export default App;

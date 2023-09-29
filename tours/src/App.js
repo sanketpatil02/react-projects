d257d25bd8d3639919b03f7915fea5e318a184cf
Sanket Patil <sanketpatil9902@gmail.com>
2023-09-29 23:56:04 +0530

    added code

--- tours/src/App.js
+++ tours/src/App.js
@@ -6,8 +6,40 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 function App() {
   const [loading, setLoading] = useState(true);
-  const [tour, setTour] = useState([])
-  return <h2>Tours Project Setup</h2>
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try{
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch(error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  useEffect( ()=>{
+    fetchTours();
+  }, []);
+
+  if(loading){
+    return(
+      <main>
+        <Loading />
+      </main>
+    )
+
+  }
+
+  return(
+    <main>
+      <Tours tours={tours} />
+    </main>
+  )
 }
 
 export default App
--- tours/src/Tours.js
+++ tours/src/Tours.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours}) => {
+  
 };
 
 export default Tours;

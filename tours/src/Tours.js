84d41e834373f5068b279a8977c52c1135103739
Sanket Patil <sanketpatil9902@gmail.com>
2023-10-04 22:58:54 +0530

    tours display data

--- tours/src/Tours.js
+++ tours/src/Tours.js
@@ -2,7 +2,19 @@ import React from 'react';
 import Tour from './Tour';
 const Tours = ({tours}) => {
   return(
-    <h2>Tours comp</h2>
+    <section>
+      <div className='title'>
+        <h2>our tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return(
+            <Tour key={tour.id} {...tour} />
+          )
+        })}
+      </div>
+    </section>
   )
 };
 

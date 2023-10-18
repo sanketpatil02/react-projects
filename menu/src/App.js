9feb36f26b5d1ae8b880ff292066e45f9d0e5b86
Sanket Patil <sanketpatil9902@gmail.com>
2023-10-18 20:11:50 +0530

    modified app.js

--- menu/src/App.js
+++ menu/src/App.js
@@ -3,10 +3,22 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const allCategories = ['all', ...new Set(items.map( (item) => item.category))];
+// console.log(allCategories)
+
 function App() {
 
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all')
+      setCategories(items);
+    else{
+      const newItems = items.filter((item) => item.category === category);
+      setCategories(newItems);
+    }
+  }
 
   return(
     <main>
@@ -17,7 +29,7 @@ function App() {
         </div>
       </section>
 
-      <Categories />
+      <Categories categories={categories} filterItems={filterItems} />
       <Menu items={menuItems}/>
     </main>
   )
--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems, categories}) => {
+  return ;
 };
 
 export default Categories;
